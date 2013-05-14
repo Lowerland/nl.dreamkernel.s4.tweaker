776f36b549b7e838960cdf81d31523f5abe7f490
Lowerland <Lowerland@hotmail.com>
2013-05-14 18:54:29 +0200

    Added About Layout,main prefs,new icons

---
+++ src/pkg/talu/lowa/s4/tweaker/about/About.java
@@ -0,0 +1,20 @@
+package pkg.talu.lowa.s4.tweaker.about;
+
+import pkg.talu.lowa.s4.tweaker.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class About extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.about);
+		setTitle(R.string.menu_about);
+
+	
+	}
+	
+	
+
+}
