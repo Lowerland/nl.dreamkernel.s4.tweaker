a3a12db4fde11a8197e351b00b38c7265563a0a0
Lowerland <Lowerland@hotmail.com>
2013-05-24 02:37:51 +0200

    Updated some code for later use also

--- src/nl/dreamkernel/s4/tweaker/Main.java
+++ src/nl/dreamkernel/s4/tweaker/Main.java
@@ -17,26 +17,27 @@
 package nl.dreamkernel.s4.tweaker;
 
 import nl.dreamkernel.s4.tweaker.R;
+import android.app.Activity;
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
-import android.view.Menu;
 
-public class Main extends PreferenceActivity {
+public class Main extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
 		//setContentView(R.layout.main);
+		//getActionBar().hide();
 	}
 
+	/*
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
-	}
+	}*/
 	
 	public static class MyPreferenceFragment extends PreferenceFragment
     {
