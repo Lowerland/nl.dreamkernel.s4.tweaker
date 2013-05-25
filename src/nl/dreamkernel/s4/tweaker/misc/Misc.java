44c338c0f61676e8778ca7a5a15760bd11235b20
Lowerland <Lowerland@hotmail.com>
2013-05-25 06:21:48 +0200

    Internal/External Schedular made working + Vibrator function added

--- src/nl/dreamkernel/s4/tweaker/misc/Misc.java
+++ src/nl/dreamkernel/s4/tweaker/misc/Misc.java
@@ -13,31 +13,42 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.RadioButton;
+import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.Toast;
 
-public class Misc extends Activity {
-
+public class Misc extends Activity  {
+	    
 	// variables for the Textviews
 	private TextView InternalValue;
 	private TextView ExternalValue;
 
 	// Variables for file paths
-	//private static final SysFs vCheck_internalscheduler = new SysFs("/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/queue/scheduler");
-	//private static final SysFs vCheck_externalscheduler = new SysFs("/sys/devices/platform/msm_sdcc.2/mmc_host/mmc2/mmc2:0002/block/mmcblk1/queue/scheduler");
-	private static final SysFs vCheck_internalscheduler = new SysFs("/data/data/nl.dreamkernel.s4.tweaker/files/internalscheduler");
-	private static final SysFs vCheck_externalscheduler = new SysFs("/data/data/nl.dreamkernel.s4.tweaker/files/externalscheduler");
-	
-	
+	private static final SysFs vCheck_internalscheduler = new SysFs("/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/queue/scheduler");
+	private static final SysFs vCheck_externalscheduler = new SysFs("/sys/devices/platform/msm_sdcc.2/mmc_host/mmc2/mmc2:0002/block/mmcblk1/queue/scheduler");
+	private static final SysFs vCheck_vibrator_intensity = new SysFs("/sys/vibrator/pwm_val");
+	//private static final SysFs vCheck_internalscheduler = new SysFs("/data/data/nl.dreamkernel.s4.tweaker/files/internalscheduler");
+	//private static final SysFs vCheck_externalscheduler = new SysFs("/data/data/nl.dreamkernel.s4.tweaker/files/externalscheduler");
+		
+	// variables storing the real file values
 	private String file_value_internal;
-	private String file_value_temp;
-
-	private String file_value_external;
+	private String file_value_external;	
+	private String file_value_temp;	
 	private String file_value_temp2;
-	
-	// Variables
+	private int value_vibrator;
+	private int value_vibrator_temp;
 
+	
+	//the seek bar variable
+	private SeekBar seekbar_vibrator;
+		
+	// declare text label objects
+    private TextView vibratorProgress;
+	
 	// variables to store the shared pref in
 	private int InternalPrefValue;
 	private int ExternalPrefValue;
@@ -49,61 +60,81 @@ public class Misc extends Activity {
 		setTitle(R.string.menu_misc_tweaks);
 		getActionBar().hide();
 		
-
-		
-
-
-		// Read in the Values from files
-		RootProcess rootProcess = new RootProcess();
-		Log.d("MiscTweaks", "Misc Tweaks, Root init s");
-		rootProcess.init();
-		Log.d("MiscTweaks", "Misc Tweaks, Root init e");
-
-		if (vCheck_internalscheduler.exists()) {
-			file_value_temp = vCheck_internalscheduler.read(rootProcess);
-		
-			Pattern p = Pattern.compile("\\[((.*?)\\])");
-			Matcher m = p.matcher(file_value_temp);
-			if (m.find()){
-				file_value_internal = m.toMatchResult().group(0);
-				file_value_internal = file_value_internal.replace("[", "").replace("]", "");
-			}
-			
-		} else {
-			
-		}
-		
-		if (vCheck_externalscheduler.exists()) {
-			file_value_temp2 = vCheck_externalscheduler.read(rootProcess);
-			
-			Pattern a = Pattern.compile("\\[((.*?)\\])");
-			Matcher b = a.matcher(file_value_temp2);
-			if (b.find()){
-				file_value_external = b.toMatchResult().group(0);
-				file_value_external = file_value_external.replace("[", "").replace("]", "");
-			}
-		} else {
-			//gpl_speaker_gain = value_default;
-		}
-
-		rootProcess.term();
-		rootProcess = null;
-		////
-		
 		// Find current value views
 		InternalValue = (TextView)findViewById(R.id.InternalValue);
 		ExternalValue = (TextView)findViewById(R.id.ExternalValue);
-		
-		// Set current value views
-		InternalValue.setText(""+file_value_internal);
-		ExternalValue.setText(""+file_value_external);
+		vibratorProgress = (TextView)findViewById(R.id.value_vibrator_intensity);
 
+		//get the seek bar
+		seekbar_vibrator = (SeekBar) findViewById(R.id.sb_vibrator_intensity);
+		
 		//get the Shared Prefs
 		final SharedPreferences sharedPreferences = getSharedPreferences("MY_SHARED_PREF", 0);
 		InternalPrefValue = sharedPreferences.getInt("InternalPref", 0);
 		ExternalPrefValue = sharedPreferences.getInt("ExternalPref", 0);
 		
+		// read the files value
+		ValueReader();
 		
+ 		//set progress text
+		vibratorProgress.setText(""+value_vibrator);
+		
+        //set the seek bar progress
+		seekbar_vibrator.setKeyProgressIncrement(value_vibrator);
+                 
+        //sets the progress of the seek bar
+		seekbar_vibrator.setProgress(value_vibrator);
+		
+		//register OnSeekBarChangeListener, so it can actually change values
+		seekbar_vibrator.setOnSeekBarChangeListener(new OnSeekBarChangeListener() 
+      		{
+      			@Override
+      			public void onStopTrackingTouch(SeekBar seekBar) 
+      			{
+      			  //set the preferences using the seekbar_gpl variable value
+      			 /* SharedPreferences sharedPreferences = getSharedPreferences("MY_SHARED_PREF", 0);
+      			  SharedPreferences.Editor editor = sharedPreferences.edit();
+      			  editor.putInt("gpl_speaker_gain", gpl_speaker_gain);
+      			  editor.commit(); */
+      			  
+      			// Try catch block for if it may go wrong 
+      			try {
+      				Log.d("process","vibrator: "+ value_vibrator);
+      				//calls RootProcess
+					RootProcess process = new RootProcess();
+					if (!process.init()) {
+					    return;
+					}
+					// Writing the values to the files 
+					process.write("echo "+ value_vibrator +" > /sys/vibrator/pwm_val\n");
+					Log.d("process","echo value_vibrator : "+ value_vibrator);
+					process.term();
+				} catch (Exception e) {
+					Log.e("Error","crashed"+e);
+					Log.d("Error","error"+e);
+				}
+      			  
+
+      			}
+      			
+      			@Override
+      			public void onStartTrackingTouch(SeekBar seekBar) 
+      			{
+      			}
+     			
+      			@Override
+      			public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) 
+      			{
+      				// sets the minimal level
+      				int siz=progress;
+      				value_vibrator = siz;
+      				Log.d("progress","Progressbar: "+siz);
+      				Log.d("progress","Progressbar: "+value_vibrator);
+     				
+      				// change progress text label with current seekbar value
+      				vibratorProgress.setText(""+value_vibrator);
+      			}
+      		});	
 		
 		// Dropdown menu for I/O Scheduler Internal
 		Spinner sInternal = (Spinner) findViewById(R.id.internalspinner);
@@ -123,6 +154,50 @@ public class Misc extends Activity {
       			editor.putInt("InternalPref", position);
       			editor.commit(); 
 				Log.d("internaladapter","internaladapter: position=" + position + " id=" + id);
+				
+      			// Try catch block for if it may go wrong 
+      			try {
+      				//calls RootProcess
+					RootProcess process = new RootProcess();
+					if (!process.init()) {
+					    return;
+					}
+					// Writing the selected value to file 
+					switch (position){				     
+				    case 0:
+				    	process.write("echo noop > /sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/queue/scheduler\n");
+				    	Log.d("process","echo'd noop to internalscheduler");
+						break;
+				    case 1:
+				    	process.write("echo deadline > /sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/queue/scheduler\n");
+				    	Log.d("process","echo'd deadline to internalscheduler");
+				    	break;
+				    case 2:
+				    	process.write("echo cfq > /sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/queue/scheduler\n");
+				    	Log.d("process","echo'd cfq to internalscheduler");
+				    	break;
+				    case 3:
+				    	process.write("echo bfq > /sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/queue/scheduler\n");
+				    	Log.d("process","echo'd bfq to internalscheduler");
+				    	break;
+				    case 4:
+				    	process.write("echo fiops > /sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/queue/scheduler\n");
+				    	Log.d("process","echo'd fiops to internalscheduler");
+				    	break;
+				    case 5:
+				    	process.write("echo sio > /sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/queue/scheduler\n");
+				    	Log.d("process","echo'd sio to internalscheduler");
+				    	break;
+				    default:
+				    	break;
+				     }
+
+					process.term();
+					ValueReader();
+				} catch (Exception e) {
+					Log.e("Error","crashed"+e);
+					Log.d("Error","error"+e);
+				}
 			}
 
 			public void onNothingSelected(AdapterView<?> parent) {
@@ -152,13 +227,99 @@ public class Misc extends Activity {
       			editor.putInt("ExternalPref", position);
       			editor.commit(); 
 				Log.d("externaladapter","externaladapter: position=" + position + " id=" + id);
+				
+      				//calls RootProcess
+					RootProcess process = new RootProcess();
+					if (!process.init()) {
+					    return;
+					}
+					// Writing the selected value to file 
+					switch (position){				     
+				    case 0:
+						process.write("echo noop > /sys/devices/platform/msm_sdcc.2/mmc_host/mmc2/mmc2:0002/block/mmcblk1/queue/scheduler\n");
+						Log.d("process","echo'd noop to externalscheduler");
+				    	break;
+				    case 1:
+				    	process.write("echo deadline > /sys/devices/platform/msm_sdcc.2/mmc_host/mmc2/mmc2:0002/block/mmcblk1/queue/scheduler\n");
+				       	Log.d("process","echo'd deadline to externalscheduler");
+				    	break;
+				    case 2:
+				    	process.write("echo cfq > /sys/devices/platform/msm_sdcc.2/mmc_host/mmc2/mmc2:0002/block/mmcblk1/queue/scheduler\n");
+				       	Log.d("process","echo'd cfq to externalscheduler");
+				    	break;
+				    case 3:
+				    	process.write("echo bfq > /sys/devices/platform/msm_sdcc.2/mmc_host/mmc2/mmc2:0002/block/mmcblk1/queue/scheduler\n");
+				       	Log.d("process","echo'd bfq to externalscheduler");
+				    	break;
+				    case 4:
+				    	process.write("echo fiops > /sys/devices/platform/msm_sdcc.2/mmc_host/mmc2/mmc2:0002/block/mmcblk1/queue/scheduler\n");
+				       	Log.d("process","echo'd fiops to externalscheduler");
+				    	break;
+				    case 5:
+				    	process.write("echo sio > /sys/devices/platform/msm_sdcc.2/mmc_host/mmc2/mmc2:0002/block/mmcblk1/queue/scheduler\n");
+				       	Log.d("process","echo'd sio to externalscheduler");
+				    	break;
+				    default:
+				    	break;
+				     }
+
+					process.term();		
+					ValueReader();
+				
 			}
 
 			public void onNothingSelected(AdapterView<?> parent) {
 				Log.d("externaladapter","externaladapter: Nothing selected");
 			}
 		});
+		
 
 	}
+	
+	void showToast(CharSequence msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
 
+	private void ValueReader(){
+		// Read in the Values from files
+		RootProcess rootProcess = new RootProcess();
+		Log.d("MiscTweaks", "Misc Tweaks, Root init s");
+		rootProcess.init();
+		Log.d("MiscTweaks", "Misc Tweaks, Root init e");
+
+		if (vCheck_internalscheduler.exists()) {
+			file_value_temp = vCheck_internalscheduler.read(rootProcess);
+		
+			Pattern p = Pattern.compile("\\[((.*?)\\])");
+			Matcher m = p.matcher(file_value_temp);
+			if (m.find()){
+				file_value_internal = m.toMatchResult().group(0);
+				file_value_internal = file_value_internal.replace("[", "").replace("]", "");
+			}			
+		} else { file_value_internal = "File Not Found"; 	}
+		
+		if (vCheck_externalscheduler.exists()) {
+			file_value_temp2 = vCheck_externalscheduler.read(rootProcess);
+			
+			Pattern a = Pattern.compile("\\[((.*?)\\])");
+			Matcher b = a.matcher(file_value_temp2);
+			if (b.find()){
+				file_value_external = b.toMatchResult().group(0);
+				file_value_external = file_value_external.replace("[", "").replace("]", "");
+			}
+		} else { file_value_external = "File Not Found";	}
+		
+		 if (vCheck_vibrator_intensity.exists()) {
+			 value_vibrator_temp = Integer.parseInt(vCheck_vibrator_intensity.read(rootProcess));
+			 value_vibrator = value_vibrator_temp;
+	        } else { value_vibrator = 0; }
+
+		rootProcess.term();
+		rootProcess = null;
+		////
+		
+		// Set current value views
+		InternalValue.setText(""+file_value_internal);
+		ExternalValue.setText(""+file_value_external);
+		 }
 }
