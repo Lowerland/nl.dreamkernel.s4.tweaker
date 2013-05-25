8daabfe53dd8477479f3056ccebd13e22ad25c71
Lowerland <Lowerland@hotmail.com>
2013-05-25 16:20:28 +0200

    Added New functions for CPU Min/Max Freq options

--- src/nl/dreamkernel/s4/tweaker/cpu/CpuTweaks.java
+++ src/nl/dreamkernel/s4/tweaker/cpu/CpuTweaks.java
@@ -35,17 +35,30 @@ public class CpuTweaks extends Activity {
 	
 	// variables for the Textviews
 	private TextView CpuCurrentValue;
+	private TextView CpuMinFREQValue;
+	private TextView CpuMaxFREQValue;
 	
 	// Variables for file paths
 	private static final SysFs vCheck_CPU_GOVERNOR = new SysFs("/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor");
+	private static final SysFs vCheck_CPU_CpuMinFREQ = new SysFs("/sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq");
+	private static final SysFs vCheck_CPU_CpuMaxFREQ = new SysFs("/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq");
 	//private static final SysFs vCheck_CPU_GOVERNOR = new SysFs("/data/data/nl.dreamkernel.s4.tweaker/files/scaling_governor");
-
+	//private static final SysFs vCheck_CPU_CpuMinFREQ = new SysFs("/data/data/nl.dreamkernel.s4.tweaker/files/scaling_min_freq");
+	//private static final SysFs vCheck_CPU_CpuMaxFREQ = new SysFs("/data/data/nl.dreamkernel.s4.tweaker/files/scaling_max_freq");
+	
+	
 	// variables storing the real file values
 	private String file_CPU_GOVERNOR;
-	private String file_CPU_GOVERNOR_temp;	
+	private String file_CPU_GOVERNOR_temp;
+	private int file_CPU_MinFREQ;
+	private int file_CPU_MinFREQ_temp;
+	private int file_CPU_MaxFREQ;
+	private int file_CPU_MaxFREQ_temp;
 	
 	// variables to store the shared pref in
 	private int CpuGovernorPrefValue;
+	private int CpuMinFREQPrefValue;
+	private int CpuMaxFREQPrefValue;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -58,9 +71,16 @@ public class CpuTweaks extends Activity {
 		// Find current value views
 		CpuCurrentValue = (TextView)findViewById(R.id.CpuCurrentValue);
 		
+		CpuMinFREQValue = (TextView)findViewById(R.id.CpuMinFreqValue);
+		CpuMaxFREQValue = (TextView)findViewById(R.id.CpuMaxFreqValue);
+		
+		
 		//get the Shared Prefs
 		final SharedPreferences sharedPreferences = getSharedPreferences("MY_SHARED_PREF", 0);
 		CpuGovernorPrefValue = sharedPreferences.getInt("CpuGovernorPref", 0);
+		
+		CpuMinFREQPrefValue = sharedPreferences.getInt("CpuMinFREQPref", 0);
+		CpuMaxFREQPrefValue = sharedPreferences.getInt("CpuMaxFREQPref", 0);
 		
 		// read the files value
 		ValueReader();
@@ -141,6 +161,162 @@ public class CpuTweaks extends Activity {
 						Log.d("cpuprefadapter","cpuprefadapter: Nothing selected");
 					}
 				});
+				
+				// Dropdown menu for scaling_min_freq
+				Spinner sCPUminFREQspinner = (Spinner) findViewById(R.id.cpuminfreqspinner);
+
+				ArrayAdapter<CharSequence> cpuminfreqadapter = ArrayAdapter
+						.createFromResource(this, R.array.CPUminfreqArray,
+								android.R.layout.simple_spinner_item);
+				cpuminfreqadapter
+						.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+				sCPUminFREQspinner.setAdapter(cpuminfreqadapter);
+				//set the option based on the sharedprefs
+				sCPUminFREQspinner.setSelection(CpuMinFREQPrefValue, true);
+				sCPUminFREQspinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+					public void onItemSelected(AdapterView<?> parent, View view,
+							int position, long id) {
+				    	SharedPreferences.Editor editor = sharedPreferences.edit();
+		      			editor.putInt("CpuMinFREQPref", position);
+		      			editor.commit(); 
+						Log.d("cpuminprefadapter","CpuMinFREQPref: position=" + position + " id=" + id);
+						
+		      			// Try catch block for if it may go wrong 
+		      			try {
+		      				//calls RootProcess
+							RootProcess process = new RootProcess();
+							if (!process.init()) {
+							    return;
+							}
+							// Writing the selected value to file 
+							switch (position){				     
+						    case 0:
+						    	process.write("echo 162000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq\n");
+						    	Log.d("cpuminprefadapter","echo'd 162000 to Cpu Min FREQ");
+								break;
+						    case 1:
+						    	process.write("echo 216000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq\n");
+						    	Log.d("cpuminprefadapter","echo'd 216000 to Cpu Min FREQ");
+								break;
+						    case 2:
+						    	process.write("echo 270000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq\n");
+						    	Log.d("cpuminprefadapter","echo'd 270000 to Cpu Min FREQ");
+								break;
+						    case 3:
+						    	process.write("echo 324000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq\n");
+						    	Log.d("cpuminprefadapter","echo'd 324000 to Cpu Min FREQ");
+								break;
+						    case 4:
+						    	process.write("echo 378000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq\n");
+						    	Log.d("cpuminprefadapter","echo'd 378000 to Cpu Min FREQ");
+								break;
+						    case 5:
+						    	process.write("echo 384000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq\n");
+						    	Log.d("cpuminprefadapter","echo'd 384000 to Cpu Min FREQ");
+								break;
+						    case 6:
+						    	process.write("echo 486000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq\n");
+						    	Log.d("cpuminprefadapter","echo'd 486000 to Cpu Min FREQ");
+								break;
+						    case 7:
+						    	process.write("echo 594000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq\n");
+						    	Log.d("cpuminprefadapter","echo'd 594000 to Cpu Min FREQ");
+								break;
+						    default:
+						    	break;
+						     }
+
+							process.term();
+							ValueReader();
+						} catch (Exception e) {
+							Log.e("Error","crashed"+e);
+							Log.d("Error","error"+e);
+						}
+					}
+
+					public void onNothingSelected(AdapterView<?> parent) {
+						Log.d("cpuminprefadapter","CpuMinFREQPref: Nothing selected");
+					}
+				});
+				
+				// Dropdown menu for scaling_max_freq
+				Spinner sCPUmaxFREQspinner = (Spinner) findViewById(R.id.cpumaxfreqspinner);
+
+				ArrayAdapter<CharSequence> cpumaxfreqadapter = ArrayAdapter
+						.createFromResource(this, R.array.CPUmaxfreqArray,
+								android.R.layout.simple_spinner_item);
+				cpumaxfreqadapter
+						.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+				sCPUmaxFREQspinner.setAdapter(cpumaxfreqadapter);
+				//set the option based on the sharedprefs
+				sCPUmaxFREQspinner.setSelection(CpuMaxFREQPrefValue, true);
+				sCPUmaxFREQspinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+					public void onItemSelected(AdapterView<?> parent, View view,
+							int position, long id) {
+				    	SharedPreferences.Editor editor = sharedPreferences.edit();
+		      			editor.putInt("CpuMaxFREQPref", position);
+		      			editor.commit(); 
+						Log.d("cpumaxfreqadapter","CpuMaxFREQPref: position=" + position + " id=" + id);
+						
+		      			// Try catch block for if it may go wrong 
+		      			try {
+		      				//calls RootProcess
+							RootProcess process = new RootProcess();
+							if (!process.init()) {
+							    return;
+							}
+							// Writing the selected value to file 
+							switch (position){				     
+						    case 0:
+						    	process.write("echo 1566000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq\n");
+						    	Log.d("cpumaxfreqadapter","echo'd 1566000 to Cpu Max FREQ");
+								break;
+						    case 1:
+						    	process.write("echo 1674000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq\n");
+						    	Log.d("cpumaxfreqadapter","echo'd 1674000 to Cpu Max FREQ");
+								break;
+						    case 2:
+						    	process.write("echo 1782000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq\n");
+						    	Log.d("cpumaxfreqadapter","echo'd 1782000 to Cpu Max FREQ");
+								break;
+						    case 3:
+						    	process.write("echo 1890000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq\n");
+						    	Log.d("cpumaxfreqadapter","echo'd 1890000 to Cpu Max FREQ");
+								break;
+						    case 4:
+						    	process.write("echo 1944000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq\n");
+						    	Log.d("cpumaxfreqadapter","echo'd 1944000 to Cpu Max FREQ");
+								break;
+						    case 5:
+						    	process.write("echo 1998000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq\n");
+						    	Log.d("cpumaxfreqadapter","echo'd 1998000 to Cpu Max FREQ");
+								break;
+						    case 6:
+						    	process.write("echo 2052000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq\n");
+						    	Log.d("cpumaxfreqadapter","echo'd 2052000 to Cpu Max FREQ");
+								break;
+						    case 7:
+						    	process.write("echo 2106000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq\n");
+						    	Log.d("cpumaxfreqadapter","echo'd 2106000 to Cpu Max FREQ");
+								break;
+						    default:
+						    	break;
+						     }
+
+							process.term();
+							ValueReader();
+						} catch (Exception e) {
+							Log.e("Error","crashed"+e);
+							Log.d("Error","error"+e);
+						}
+					}
+
+					public void onNothingSelected(AdapterView<?> parent) {
+						Log.d("cpumaxfreqadapter","CpuMaxFREQPref: Nothing selected");
+					}
+				});
+				
+				
 	}
 
 	void showToast(CharSequence msg) {
@@ -158,6 +334,16 @@ public class CpuTweaks extends Activity {
 			file_CPU_GOVERNOR_temp = vCheck_CPU_GOVERNOR.read(rootProcess);
 			file_CPU_GOVERNOR = file_CPU_GOVERNOR_temp;					
 		} else { file_CPU_GOVERNOR = "File Not Found"; 	}
+		
+        if (vCheck_CPU_CpuMinFREQ.exists()) {
+        	file_CPU_MinFREQ_temp = Integer.parseInt(vCheck_CPU_CpuMinFREQ.read(rootProcess));
+        	file_CPU_MinFREQ = file_CPU_MinFREQ_temp;
+        } else { }
+        
+        if (vCheck_CPU_CpuMaxFREQ.exists()) {
+        	file_CPU_MaxFREQ_temp = Integer.parseInt(vCheck_CPU_CpuMaxFREQ.read(rootProcess));
+        	file_CPU_MaxFREQ = file_CPU_MaxFREQ_temp;
+        } else {  }
 			
 		rootProcess.term();
 		rootProcess = null;
@@ -165,6 +351,8 @@ public class CpuTweaks extends Activity {
 		
 		// Set current value views
 		CpuCurrentValue.setText(""+file_CPU_GOVERNOR);
+		CpuMinFREQValue.setText(""+file_CPU_MinFREQ);
+		CpuMaxFREQValue.setText(""+file_CPU_MaxFREQ);
 		
 		 }
 	
