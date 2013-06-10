131c244191235e8c86405632c77ba898bd2d83ae
Lowerland <Lowerland@hotmail.com>
2013-06-10 09:25:49 +0200

    Reformatted Code

--- src/nl/dreamkernel/s4/tweaker/util/RootProcess.java
+++ src/nl/dreamkernel/s4/tweaker/util/RootProcess.java
@@ -23,87 +23,87 @@ import java.io.IOException;
 import org.apache.http.protocol.HTTP;
 
 public class RootProcess {
-	
+
 	private Process mProcess = null;
-    private DataOutputStream mOutputStream = null;
-    private DataInputStream mInputStream = null;
+	private DataOutputStream mOutputStream = null;
+	private DataInputStream mInputStream = null;
 
-    public boolean init() {
-        try {
-            mProcess = Runtime.getRuntime().exec("su");
-            mOutputStream = new DataOutputStream(mProcess.getOutputStream());
-            mInputStream = new DataInputStream(mProcess.getInputStream());
-            if (write("su -v\n")) {
-                String[] results = read();
-                for (String line : results) {
-                    if (line.length() > 0) {
-                        return true;
-                    }
-                }
-            }
-        } catch (IOException e) {
-        }
-        return false;
-    }
+	public boolean init() {
+		try {
+			mProcess = Runtime.getRuntime().exec("su");
+			mOutputStream = new DataOutputStream(mProcess.getOutputStream());
+			mInputStream = new DataInputStream(mProcess.getInputStream());
+			if (write("su -v\n")) {
+				String[] results = read();
+				for (String line : results) {
+					if (line.length() > 0) {
+						return true;
+					}
+				}
+			}
+		} catch (IOException e) {
+		}
+		return false;
+	}
 
-    public void term() {
-        if (mInputStream != null) {
-            try {
-                mInputStream.close();
-            } catch (IOException e) {
-            }
-        }
-        if (mOutputStream != null) {
-            try {
-                if (mProcess != null) {
-                    mOutputStream.writeBytes("exit\n");
-                    mOutputStream.flush();
-                    try {
-                        mProcess.waitFor();
-                    } catch (InterruptedException e) {
-                    }
-                }
-                mOutputStream.close();
-            } catch (IOException e) {
-            }
-        }
-        if(mProcess != null){
-            mProcess.destroy();
-        }
-        mOutputStream = null;
-        mInputStream = null;
-        mProcess = null;
-    }
+	public void term() {
+		if (mInputStream != null) {
+			try {
+				mInputStream.close();
+			} catch (IOException e) {
+			}
+		}
+		if (mOutputStream != null) {
+			try {
+				if (mProcess != null) {
+					mOutputStream.writeBytes("exit\n");
+					mOutputStream.flush();
+					try {
+						mProcess.waitFor();
+					} catch (InterruptedException e) {
+					}
+				}
+				mOutputStream.close();
+			} catch (IOException e) {
+			}
+		}
+		if (mProcess != null) {
+			mProcess.destroy();
+		}
+		mOutputStream = null;
+		mInputStream = null;
+		mProcess = null;
+	}
 
-    public String[] read() {
-        if (mInputStream != null) {
-            String ret = "";
-            int size = 0;
-            byte[] buffer = new byte[1024];
-            try {
-                do {
-                    size = mInputStream.read(buffer);
-                    if (size > 0) {
-                        ret += new String(buffer, 0, size, HTTP.UTF_8);
-                    }
-                } while (mInputStream.available() > 0);
-            } catch (IOException e) {
-            }
-            return ret.split("\n");
-        }
-        return null;
-    }
+	public String[] read() {
+		if (mInputStream != null) {
+			String ret = "";
+			int size = 0;
+			byte[] buffer = new byte[1024];
+			try {
+				do {
+					size = mInputStream.read(buffer);
+					if (size > 0) {
+						ret += new String(buffer, 0, size, HTTP.UTF_8);
+					}
+				} while (mInputStream.available() > 0);
+			} catch (IOException e) {
+			}
+			return ret.split("\n");
+		}
+		return null;
+	}
 
-    public boolean write(String command) {
-        if (mOutputStream != null) {
-            try {
-                mOutputStream.writeBytes(command);
-                mOutputStream.flush();
-                return true;
-            } catch (IOException e) {
-            }
-        }
-        return false;
-    }
+	public boolean write(String command) {
+		if (mOutputStream != null) {
+			try {
+				mOutputStream.writeBytes(command);
+				mOutputStream.flush();
+				return true;
+			} catch (IOException e) {
+			}
+		}
+		return false;
+	}
 
 }
