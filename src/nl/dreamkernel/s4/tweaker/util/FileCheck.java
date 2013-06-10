3e2278f9c48dfc8ae7940526ae64863071801d59
Lowerland <Lowerland@hotmail.com>
2013-06-10 09:24:48 +0200

    Reformatted Code

--- src/nl/dreamkernel/s4/tweaker/util/FileCheck.java
+++ src/nl/dreamkernel/s4/tweaker/util/FileCheck.java
@@ -24,13 +24,11 @@ import nl.dreamkernel.s4.tweaker.misc.Misc;
 import nl.dreamkernel.s4.tweaker.soundtweaks.SoundTweaks;
 import nl.dreamkernel.s4.tweaker.util.SysFs;
 
-
 public class FileCheck {
-	
+
 	// Set File Constants
 	public static final SysFs vCheck_BUSYBOX = new SysFs("/system/xbin/busybox");
-	
-	
+
 	// Initialize Variables for SoundTweaks
 	public static int gpl_spk_hide;
 	public static int gpl_mic_hide;
@@ -49,72 +47,73 @@ public class FileCheck {
 	public static int Usb_Fast_charge_hide;
 	// Initialize Boolean AlertDialog
 	public static boolean incompatible;
-	//////
+
+	// ////
 	// FileChecking Methods
-	/////
+	// ///
 
 	// SoundTweaks FileChecking Method
 	public static void CheckSoundOptions(Context context) {
-		
+
 		incompatible = false;
 		gpl_spk_hide = 0;
 		gpl_mic_hide = 0;
 		gpl_cam_mic_hide = 0;
 		gpl_headphone_hide = 0;
 		gpl_hdmi_spkr_hide = 0;
-		gpl_headset_mic_hide = 0;		
-		
+		gpl_headset_mic_hide = 0;
+
 		if (!SoundTweaks.vCheck_gpl_speaker_gain.exists()) {
 			gpl_spk_hide = 1;
 			incompatible = true;
-	    } 
+		}
 		if (!SoundTweaks.vCheck_gpl_mic_gain.exists()) {
-	    	gpl_mic_hide = 1;
-			incompatible = true;	    	
-	    }
+			gpl_mic_hide = 1;
+			incompatible = true;
+		}
 		if (!SoundTweaks.vCheck_gpl_cam_mic_gain.exists()) {
 			gpl_cam_mic_hide = 1;
-			incompatible = true;	    	
-	    }
+			incompatible = true;
+		}
 		if (!SoundTweaks.vCheck_gpl_headphone_gain.exists()) {
 			gpl_headphone_hide = 1;
-			incompatible = true;	    	
-	    }
+			incompatible = true;
+		}
 		if (!SoundTweaks.vCheck_gpl_hdmi_speaker_gain.exists()) {
 			gpl_hdmi_spkr_hide = 1;
-			incompatible = true;	    	
-	    }
+			incompatible = true;
+		}
 		if (!SoundTweaks.vCheck_gpl_headset_mic_gain.exists()) {
 			gpl_headset_mic_hide = 1;
-			incompatible = true;	    	
-	    }
+			incompatible = true;
+		}
 	}
-	
+
 	// CpuTweaks FileChecking Method
 	public static void CheckCPUOptions(Context context) {
-		
+
 		incompatible = false;
 		cpuGovernor_hide = 0;
 		cpuMinFreq_hide = 0;
 		cpuMaxFreq_hide = 0;
-		
+
 		if (!CpuTweaks.vCheck_CPU_GOVERNOR.exists()) {
 			cpuGovernor_hide = 1;
 			incompatible = true;
-			}
+		}
 		if (!CpuTweaks.vCheck_CPU_CpuMinFREQ.exists()) {
 			cpuMinFreq_hide = 1;
 			incompatible = true;
-			}
+		}
 		if (!CpuTweaks.vCheck_CPU_CpuMaxFREQ.exists()) {
 			cpuMaxFreq_hide = 1;
 			incompatible = true;
-			}
 		}
+	}
 
 	// MiscTweaks FileChecking Method
 	public static void CheckMiscOptions(Context context) {
-		
+
 		incompatible = false;
 		internal_scheduler_hide = 0;
 		external_scheduler_hide = 0;
@@ -124,31 +123,31 @@ public class FileCheck {
 		if (!Misc.vCheck_internalscheduler.exists()) {
 			internal_scheduler_hide = 1;
 			incompatible = true;
-			}
-		if (!Misc.vCheck_externalscheduler.exists()) {  // <--- TEMP DISABLED
+		}
+		if (!Misc.vCheck_externalscheduler.exists()) { // <--- TEMP DISABLED
 			external_scheduler_hide = 1;
 			incompatible = true;
-			}
+		}
 		if (!Misc.vCheck_vibrator_intensity.exists()) {
 			vibrator_intensity_hide = 1;
 			incompatible = true;
-			}
+		}
 		if (!Misc.vCheck_Usb_Fast_charge.exists()) {
 			Usb_Fast_charge_hide = 1;
 			incompatible = true;
-			}
+		}
 	}
-	
+
 	// Root acces check
 	public static boolean isRootEnabled() {
-        File file = new File("/system/bin/su");
-        if (file.exists()) {
-            return true;
-        }
-        File file2 = new File("/system/xbin/su");
-        if (file2.exists()) {
-            return true;
-        }
-        return false;
-    }
+		File file = new File("/system/bin/su");
+		if (file.exists()) {
+			return true;
+		}
+		File file2 = new File("/system/xbin/su");
+		if (file2.exists()) {
+			return true;
+		}
+		return false;
+	}
 }
