3318dff1dbe275669aedf11a6b2e0561762febc7
Lowerland <Lowerland@hotmail.com>
2013-06-10 09:24:06 +0200

    Reformatted Code

--- src/nl/dreamkernel/s4/tweaker/util/DialogActivity.java
+++ src/nl/dreamkernel/s4/tweaker/util/DialogActivity.java
@@ -31,77 +31,71 @@ import android.widget.CompoundButton.OnCheckedChangeListener;
 
 public class DialogActivity extends Activity implements OnCheckedChangeListener {
 	static final String TAG = "S4Tweaker";
-	
+
 	public static int hidedialog;
 	private CheckBox checkBoxHIDEALERT;
-	
+
 	private Button button_ok;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		getWindow().requestFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.dialog_alert);
-		
+
 		hidedialog = 0;
-		checkBoxHIDEALERT = (CheckBox)findViewById(R.id.checkBoxHIDEALERT);
+		checkBoxHIDEALERT = (CheckBox) findViewById(R.id.checkBoxHIDEALERT);
 		checkBoxHIDEALERT.setOnCheckedChangeListener(this);
-		
-		button_ok = (Button)findViewById(R.id.btn_dialog_ok);
+
+		button_ok = (Button) findViewById(R.id.btn_dialog_ok);
 		button_ok.setOnClickListener(buttonDialogListener);
 	}
 
 	public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-			if (isChecked) {
-				
-				Log.d(TAG,"isChecked");
-				hidedialog = 1;
-				//setResult(RESULT_OK, (new Intent()).setAction("checkbox Corky!"));
-	            //finish();
-				/*hide_dialog = 1;
-				SharedPreferences sharedPreferences = getSharedPreferences("MY_SHARED_PREF", 0);
-				SharedPreferences.Editor editor = sharedPreferences.edit();
-				editor.putInt("hide_dialog", hide_dialog);
-				editor.commit();*/ 
-				
-				//Log.d(TAG,"Editor "+hide_dialog);
-			} else {
-				
-				//setResult(RESULT_OK, (new Intent()).setAction("checkbox false"));
-				hidedialog = 0;
-				/*hide_dialog = 0;
-				Log.d(TAG,"isunChecked"+hide_dialog);*/
-			}
-		
-	}
-	
-	
-	/*@Override
-	public void onClick(View v) {
-		// TODO Auto-generated method stub
-		finish();
-		Log.d(TAG,"onDialogOK pressed");
-	}*/
-    private OnClickListener buttonDialogListener = new OnClickListener()
-    {
-        public void onClick(View v)
-        {
-            // To send a result, simply call setResult() before your
-            // activity is finished.
-            setResult(RESULT_OK, (new Intent()).setAction(""+hidedialog));
-            finish();
-        }
-    };
+		if (isChecked) {
 
-    /*private OnClickListener mVioletListener = new OnClickListener()
-    {
-        public void onClick(View v)
-        {
-            // To send a result, simply call setResult() before your
-            // activity is finished.
-            setResult(RESULT_OK, (new Intent()).setAction("Violet!"));
-            finish();
-        }
-    };*/
+			Log.d(TAG, "isChecked");
+			hidedialog = 1;
+			// setResult(RESULT_OK, (new
+			// Intent()).setAction("checkbox Corky!"));
+			// finish();
+			/*
+			 * hide_dialog = 1; SharedPreferences sharedPreferences =
+			 * getSharedPreferences("MY_SHARED_PREF", 0);
+			 * SharedPreferences.Editor editor = sharedPreferences.edit();
+			 * editor.putInt("hide_dialog", hide_dialog); editor.commit();
+			 */
+
+			// Log.d(TAG,"Editor "+hide_dialog);
+		} else {
+
+			// setResult(RESULT_OK, (new Intent()).setAction("checkbox false"));
+			hidedialog = 0;
+			/*
+			 * hide_dialog = 0; Log.d(TAG,"isunChecked"+hide_dialog);
+			 */
+		}
+
+	}
+
+	/*
+	 * @Override public void onClick(View v) { // TODO Auto-generated method
+	 * stub finish(); Log.d(TAG,"onDialogOK pressed"); }
+	 */
+	private OnClickListener buttonDialogListener = new OnClickListener() {
+		public void onClick(View v) {
+			// To send a result, simply call setResult() before your
+			// activity is finished.
+			setResult(RESULT_OK, (new Intent()).setAction("" + hidedialog));
+			finish();
+		}
+	};
+
+	/*
+	 * private OnClickListener mVioletListener = new OnClickListener() { public
+	 * void onClick(View v) { // To send a result, simply call setResult()
+	 * before your // activity is finished. setResult(RESULT_OK, (new
+	 * Intent()).setAction("Violet!")); finish(); } };
+	 */
 }
